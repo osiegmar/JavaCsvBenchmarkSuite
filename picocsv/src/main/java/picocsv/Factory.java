478ac07e5a7d67d53205d1d2fbcb4d3d4ae4d810
Philippe Charles <philippe.charles@nbb.be>
2024-10-27 14:16:40 +0100

    Fix checkstyle errors

--- picocsv/src/main/java/picocsv/Factory.java
+++ picocsv/src/main/java/picocsv/Factory.java
@@ -13,42 +13,37 @@ import nbbrd.picocsv.Csv;
 
 public final class Factory {
 
+    private static final Csv.Format FORMAT = Csv.Format.DEFAULT
+            .toBuilder()
+            .delimiter(CsvConstants.SEPARATOR)
+            .separator(Csv.Format.UNIX_SEPARATOR)
+            .quote(CsvConstants.DELIMITER)
+            .build();
+
+    // Read performances seems highly related to the buffer size
+    // Slower: the default buf size (8192) is too big for the benchmark data
+    private static final int BUF_SIZE_SLOWER = Csv.DEFAULT_CHAR_BUFFER_SIZE;
+    // Equivalent: a random low buf size seems efficient
+    private static final int BUF_SIZE_EQUIVALENT = 200;
+    // Faster: a perfect buf size (163) aligns the stars and performs very well
+    private static final int BUF_SIZE_FASTER = CsvConstants.DATA.length();
+
     private Factory() {
     }
 
     public static ICsvReader reader() throws IOException {
         return new ICsvReader() {
-
-            // Performances seems highly related to the buffer size
-
-            // Slower: the default buf size (8192) is too big for the benchmark data
-            private final int BUF_SIZE_SLOWER = Csv.DEFAULT_CHAR_BUFFER_SIZE;
-
-            // Equivalent: a random low buf size seems efficient
-            private final int BUF_SIZE_EQUIVALENT = 200;
-
-            // Faster: a perfect buf size (163) aligns the stars and performs very well
-            private final int BUF_SIZE_FASTER = CsvConstants.DATA.length();
-
-            private final Csv.Reader csvReader = Csv.Reader.of(
-                    Csv.Format.DEFAULT
-                            .toBuilder()
-                            .delimiter(CsvConstants.SEPARATOR)
-                            .separator(Csv.Format.UNIX_SEPARATOR)
-                            .quote(CsvConstants.DELIMITER)
-                            .build(),
-                    Csv.ReaderOptions.DEFAULT
-                            .toBuilder()
-                            .build(),
-                    new InfiniteDataReader(CsvConstants.DATA),
-                    BUF_SIZE_SLOWER
-            );
+            private final Csv.Reader csvReader = Csv.Reader.of(FORMAT, Csv.ReaderOptions.DEFAULT,
+                    new InfiniteDataReader(CsvConstants.DATA), BUF_SIZE_SLOWER);
 
             @Override
             public List<String> readRecord() throws IOException {
-                // picocsv is not designed to retrieve all fields at once
-                List<String> result = new ArrayList<>();
-                if (csvReader.readLine()) while (csvReader.readField()) result.add(csvReader.toString());
+                final List<String> result = new ArrayList<>();
+                if (csvReader.readLine()) {
+                    while (csvReader.readField()) {
+                        result.add(csvReader.toString());
+                    }
+                }
                 return result;
             }
 
@@ -56,28 +51,17 @@ public final class Factory {
             public void close() throws IOException {
                 csvReader.close();
             }
-
         };
     }
 
     public static ICsvWriter writer(final Writer writer) throws IOException {
         return new ICsvWriter() {
-
-            private final Csv.Writer csvWriter = Csv.Writer.of(
-                    Csv.Format.DEFAULT
-                            .toBuilder()
-                            .delimiter(CsvConstants.SEPARATOR)
-                            .separator(Csv.Format.UNIX_SEPARATOR)
-                            .quote(CsvConstants.DELIMITER)
-                            .build(),
-                    Csv.WriterOptions.DEFAULT,
-                    writer,
-                    Csv.DEFAULT_CHAR_BUFFER_SIZE
-            );
+            private final Csv.Writer csvWriter = Csv.Writer.of(FORMAT, Csv.WriterOptions.DEFAULT,
+                    writer, Csv.DEFAULT_CHAR_BUFFER_SIZE);
 
             @Override
             public void writeRecord(final List<String> fields) throws IOException {
-                for (String field : fields) {
+                for (final String field : fields) {
                     csvWriter.writeField(field);
                 }
                 csvWriter.writeEndOfLine();
@@ -87,8 +71,6 @@ public final class Factory {
             public void close() throws IOException {
                 csvWriter.close();
             }
-
         };
     }
-
 }
